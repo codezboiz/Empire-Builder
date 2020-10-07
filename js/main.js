ad6445fc2a82478439d32f173c4c6db9c1af1b24
codezboiz <60275475+codezboiz@users.noreply.github.com>
2020-10-07 15:46:24 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,4 +2,4 @@ var Harvest = [[../images/Harvest.png|HARVEST]];
 var MainHTML = [[../html/main.html]];
 
 var harvestSec = MainHTML.getElementById("harvest");
-harvestSec.innerHTML = "<img src=" + Harvest + "alt='Harvest'>";
+harvestSec.innerHTML = "<img src=" + Harvest + "alt=Harvest>";
